6ff1b1d89296869dcab9eb08148de6699f52ba49
yg <yonggeol93@gmail.com>
2020-07-18 08:40:58 +0900

    sif generator code update.

--- sif_generator.py
+++ sif_generator.py
@@ -1,6 +1,7 @@
 import multiprocessing
 from collections import Counter
 
+from gensim.models import Word2Vec
 from sklearn.preprocessing import MinMaxScaler
 from scipy import linalg
 import numpy as np
@@ -10,18 +11,19 @@ from tqdm import tqdm
 #https://bab2min.tistory.com/631
 class SIFGenerator():
     
-    def __init__(self, arr_title, w2v_model, sif_parm=0.001, vector_size=100):
+    def __init__(self, sif_parm=0.001, vector_size=100):
         
-        self.arr_title = arr_title
-        self.w2v_model = w2v_model
         self.scaler = MinMaxScaler()
         
         self.sif_parm = sif_parm
         self.vector_size = vector_size
         
-        self.word_freq_dict = self._make_word_freq_dict()
-        self.total_word_freq = self._get_total_word_freq()
         
+        self.word_freq_dict = None
+        self.total_word_freq = None
+        
+        self.arr_title = None
+        self.w2v_model = None
         self.total_sif_arr_wvs = None
 
     def get_total_wvs(self):
@@ -30,15 +32,24 @@ class SIFGenerator():
     def save_total_wvs(self, save_path):
         np.savez(save_path, self.total_sif_arr_wvs)
         
-    def _make_word_freq_dict(self):
+    def get_w2v_model(self):
+        return self.w2v_model
+    
+    def save_w2v_model(self, model_path):
+        w2v_model.save(model_path)
+
+    def _get_word_vector(self, word):
+        return self.w2v_model.wv[word]
         
-        arr_token_list = list(map(lambda x : x.split(' '), self.arr_title))
+    def _make_word_freq_dict(self, arr_title):
+        
+        arr_token_list = list(map(lambda x : x.split(' '), arr_title))
         word_freq_dict = Counter([ token for token_list in arr_token_list for token in token_list])
         
-        return word_freq_dict
+        self.word_freq_dict = dict(word_freq_dict)
     
     def _get_total_word_freq(self):
-        return sum(self.word_freq_dict.values())
+        self.total_word_freq = sum(self.word_freq_dict.values())
     
     def _get_word_weight(self, word):
         
@@ -47,9 +58,6 @@ class SIFGenerator():
         
         return word_weight
     
-    def _get_word_vector(self, word):
-        return self.w2v_model.wv[word]
-    
     def _normalize(self, vector):
 
         vector = vector.reshape(-1, 1)
@@ -57,7 +65,23 @@ class SIFGenerator():
         normalized_vector = normalized_vector.reshape(1, -1)
         
         return normalized_vector
-
+        
+    def _get_first_singular_value_idx(self, arr_vector):
+        
+        S = linalg.svd(arr_vector, compute_uv=False)
+        fisrt_singular_value_idx = np.argmax(S)
+        
+        return fisrt_singular_value_idx
+    
+    def _delete_first_singular_value_col(self, total_arr_wvs):
+        print('=> process singular vetcor decomposition....')
+        fisrt_singular_value_idx = self._get_first_singular_value_idx(total_arr_wvs)
+        
+        print('=> delete first singular value columns....')
+        total_arr_wvs = np.delete(total_arr_wvs, [fisrt_singular_value_idx], axis=1)
+        
+        return total_arr_wvs
+    
     def _process_sif_wvs(self, arr_title, process_num=0):
         
         num = 0
@@ -71,20 +95,13 @@ class SIFGenerator():
             num += 1
         
         return arr_sif_wvs
-        
-    def _get_first_singular_value_idx(self, arr_vector):
-        
-        U, S, VT = linalg.svd(arr_vector)
-        fisrt_singular_value_idx = np.argmax(S)
-        
-        return fisrt_singular_value_idx
     
-    def _delete_first_singular_value_col(self, total_arr_wvs):
-        
-        fisrt_singular_value_idx = self._get_first_singular_value_idx(total_arr_wvs)
-        total_arr_wvs = np.delete(total_arr_wvs, [fisrt_singular_value_idx], axis=1)
-        
-        return total_arr_wvs
+    def make_w2v_model(self, arr_title, n_worker, window_size=5, min_count=1):
+        print('=> start training w2v model....')
+        splited_token_list = [str_token.split(' ') for str_token in arr_title]
+        w2v_model = Word2Vec(splited_token_list, size=100, window=window_size, min_count=min_count, workers=n_worker)
+        print('=> end training w2v model....')
+        self.w2v_model = w2v_model
         
     # make feature vector
     def make_sif_wvs_vector(self, query_title, avg=True, normalize=True):
@@ -107,28 +124,48 @@ class SIFGenerator():
         
         return sif_wvs_vector
 
-    def do(self, n_worker):
+    def do(self, arr_title, n_worker, svd=True):
+        
+        # make w2v_model
+        self.make_w2v_model(arr_title, n_worker=n_worker)
+        
+        # make word_freq_dict
+        self._make_word_freq_dict(arr_title)
+        
+        # get total word freq
+        self._get_total_word_freq()
         
         if n_worker > 1:
             
             # split arr with n chunks
             arr_process_num = [i for i in range(0, n_worker)]
-            arr_chunks = np.array_split(self.arr_title, n_worker)
-
+            arr_chunks = np.array_split(arr_title, n_worker)
+            
             # multi-processing
-            print('{0} process running...'.format(n_worker))
+            print('=> {0} process running...'.format(n_worker))
             pool = multiprocessing.Pool(processes = n_worker)
             results = pool.starmap(self._process_sif_wvs, zip(arr_chunks, arr_process_num))
             pool.close()
             pool.join()
-
+            
             total_sif_arr_wvs = np.vstack(results)
         
         else:
-            total_sif_arr_wvs = self._process_sif_wvs(self.arr_title)
+            total_sif_arr_wvs = self._process_sif_wvs(arr_title)
             
-        total_sif_wvs = np.ascontiguousarray(total_sif_arr_wvs).astype('float32')
-        total_sif_arr_wvs = self._delete_first_singular_value_col(total_sif_arr_wvs)
+        print('=> make_sif_wvs process is done....')
+        
+        if svd:
+            total_sif_arr_wvs = self._delete_first_singular_value_col(total_sif_arr_wvs)
+            
         self.total_sif_arr_wvs = total_sif_arr_wvs
         
         return total_sif_arr_wvs
+    
+if __name__ == '__main__':
+    
+    # test example
+    str_token_list = ['아버지 가방에 들어가신다', '이 문장은 예시 입니다.']
+    
+    sif_generator = SIFGenerator()
+    total_sif_arr_wvs = sif_generator.do(str_token_list, n_worker = 3, svd=True)
